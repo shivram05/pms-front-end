4ea936b8afffa03abd7d6a7b7329af21982d1117
Arjun Subedi <send2arjunsubedi@gmail.com>
2023-04-28 12:51:01 -0500

    Admin option of maede functional

--- src/components/viewer/ListOfOffers.js
+++ src/components/viewer/ListOfOffers.js
@@ -2,59 +2,73 @@ import React, { useEffect, useState } from "react";
 import "./ListOfOffer.css";
 import Badge from "react-bootstrap/Badge";
 import axios from "axios";
-
+import toast from "react-hot-toast";
+import Button from "react-bootstrap/Button";
 const ListOfOffers = () => {
   const [offerListData, setOfferListData] = useState([]);
 
   const [offerId, setOfferOfferId] = useState(0);
-
+  const [flag, setFlag] = useState(false);
   const acceptOffer = (offerId) => {
-    console.log("accept offer" + offerId);
-    
-     axios
-        .post("http://localhost:8080/api/v1/offers/accept/"+offerId)
-        .then((response) => {
-          setOfferListData(response.data);
-          console.log(response.data);
-        })
-        .catch((err) => console.log(err.message));
-   
-  };
-  const declineOffer = () => {};
+    // console.log("accept offer" + offerId);
 
-  const listOfferData = ()=>{
-    offerListData.map((offer) => (
+    axios
+      .post("http://localhost:8080/api/v1/offers/accept/" + offerId)
+      .then((response) => {
+        // setOfferListData(response.data);
+        console.log(response.data);
+        setFlag(!flag);
+        setOfferOfferId();
+        toast.success("Offer Accepted and Status Changed to Contingent");
+      })
+      .catch((err) => console.log(err.message));
+  };
+  const declineOffer = (declineOFferId) => {
+    axios
+      .delete("http://localhost:8080/api/v1/offers/" + declineOFferId)
+
+      .then((response) => {
+        setFlag(!flag);
+        toast.success("Offer Removed");
+      })
+
+      .catch((err) => console.log(err.message));
+    //  offerListData.filter((offer) => offer.offerId !== declineOFferId)
+  };
+
+  const listOfferData = () => {
+    return offerListData.map((offer) => (
       <tr key={offer.offerId}>
         <td>{offer.offerId}</td>
         <td>{offer.offeredPrice}</td>
 
         <td>
-          <Badge
-            pill
-            bd="success"
-            onClick={() => {
-              acceptOffer(offer.offerId);
-            }}
-          >
-            Accept
-          </Badge>{" "}
-          <Badge
-            pill
-            bd="danger"
-            onClick={declineOffer}
-            class="secondary"
-          >
-            Decline
-          </Badge>
+          <>
+            <Button
+              variant="success"
+              onClick={() => {
+                acceptOffer(offer.offerId);
+              }}
+            >
+              Accept
+            </Button>{" "}
+            <Button
+              variant="danger"
+              onClick={() => {
+                declineOffer(offer.offerId);
+              }}
+            >
+              Delete
+            </Button>
+            <toast />
+          </>
         </td>
         <td>
-          <Badge pill bd="primary">
-            {offer.property.statusEnum}
-          </Badge>
+          <Button variant="primary">{offer.property.statusEnum}</Button>
         </td>
       </tr>
-    ))
-  }
+    ));
+  };
 
   useEffect(() => {
     // userId
@@ -62,35 +76,36 @@ const ListOfOffers = () => {
       .get("http://localhost:8080/api/v1/offers/6")
       .then((response) => {
         setOfferListData(response.data);
+
         console.log(response.data);
       })
       .catch((err) => console.log(err.message));
-  }, [6]);
+  }, [flag]);
 
   // console.log(offerListData)
   const offers = () => {
     const status = "Available";
     return (
-      <table class="table">
+      <table className="table">
         <thead>
           <tr>
-            <th class="th">Offer Id</th>
-            <th class="th">Offered Price $</th>
-            <th class="th">Action</th>
-            <th class="th">Status</th>
+            <th className="th">Offer Id</th>
+            <th className="th">Offered Price $</th>
+            <th className="th">Action</th>
+            <th className="th">Status</th>
           </tr>
         </thead>
-        <tbody>
-          {listOfferData}
-        </tbody>
+        <tbody>{listOfferData()}</tbody>
       </table>
     );
   };
 
   return (
     <div>
-      List Of Offers
-      {offers()}
+      <>
+        List Of Offers
+        {offers()}
+      </>
     </div>
   );
 };
