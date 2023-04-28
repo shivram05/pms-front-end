7dc525cdf6428c22767c511e303f77fc43abbdd3
shivram <shivramshrestha01@gmail.com>
2023-04-27 20:01:12 -0700

    some owner api workd

--- src/components/viewer/ListOfOffers.js
+++ src/components/viewer/ListOfOffers.js
@@ -1,43 +1,87 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./ListOfOffer.css";
 import Badge from "react-bootstrap/Badge";
+import axios from "axios";
 
 const ListOfOffers = () => {
-  const acceptOffer = () => {};
+  const [offerListData, setOfferListData] = useState([]);
+
+  const [offerId, setOfferOfferId] = useState(0);
+
+  const acceptOffer = (offerId) => {
+    console.log("accept offer" + offerId);
+    
+     axios
+        .post("http://localhost:8080/api/v1/offers/accept/"+offerId)
+        .then((response) => {
+          setOfferListData(response.data);
+          console.log(response.data);
+        })
+        .catch((err) => console.log(err.message));
+   
+  };
   const declineOffer = () => {};
 
+  const listOfferData = ()=>{
+    offerListData.map((offer) => (
+      <tr key={offer.offerId}>
+        <td>{offer.offerId}</td>
+        <td>{offer.offeredPrice}</td>
+
+        <td>
+          <Badge
+            pill
+            bd="success"
+            onClick={() => {
+              acceptOffer(offer.offerId);
+            }}
+          >
+            Accept
+          </Badge>{" "}
+          <Badge
+            pill
+            bd="danger"
+            onClick={declineOffer}
+            class="secondary"
+          >
+            Decline
+          </Badge>
+        </td>
+        <td>
+          <Badge pill bd="primary">
+            {offer.property.statusEnum}
+          </Badge>
+        </td>
+      </tr>
+    ))
+  }
+
+  useEffect(() => {
+    // userId
+    axios
+      .get("http://localhost:8080/api/v1/offers/6")
+      .then((response) => {
+        setOfferListData(response.data);
+        console.log(response.data);
+      })
+      .catch((err) => console.log(err.message));
+  }, [6]);
+
+  // console.log(offerListData)
   const offers = () => {
     const status = "Available";
     return (
       <table class="table">
         <thead>
           <tr>
-            <th class="th">Offer Name</th>
-            <th class="th">Description</th>
-            <th class="th">Offered Price</th>
+            <th class="th">Offer Id</th>
+            <th class="th">Offered Price $</th>
             <th class="th">Action</th>
             <th class="th">Status</th>
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>Offer 1</td>
-            <td>Description of Offer 1</td>
-            <td>$19.99</td>
-            <td>
-              <Badge pill bd="success" onClick={acceptOffer}>
-                Accept
-              </Badge>{" "}
-              <Badge pill bd="danger" onClick={declineOffer} class="secondary">
-                Decline
-              </Badge>
-            </td>
-            <td>
-              <Badge pill bd="primary">
-                {status}
-              </Badge>
-            </td>
-          </tr>
+          {listOfferData}
         </tbody>
       </table>
     );
--- src/pages/owner/OfferPages.js
+++ src/pages/owner/OfferPages.js
@@ -8,6 +8,7 @@ function OfferPages() {
   return (
     <Container>
       <Header />
+      
       <OfferContainer />
       <Footer />
     </Container>
